60f7dc91681dd4aa2a322a1b23aec9202e6de9b0
DanteHerzlos <streat13@gmail.com>
2023-09-19 20:40:56 +0300

    add: readonly for virtualized autocomplete

--- src/Autocomplete/components/VirtualAutocomplete.tsx
+++ src/Autocomplete/components/VirtualAutocomplete.tsx
@@ -1,4 +1,11 @@
-import { forwardRef, useDeferredValue, useEffect, useImperativeHandle, useRef, useState } from "react";
+import {
+  forwardRef,
+  useDeferredValue,
+  useEffect,
+  useImperativeHandle,
+  useRef,
+  useState,
+} from "react";
 import cl from "../styles/components/Autocomplete.module.css";
 import { OptionType, GroupBase } from "../types/AutocompleteTypes";
 import Input, { InputRef } from "./UI/Input";
@@ -7,6 +14,7 @@ import GroupedVirtualList from "./UI/GroupedVirtualList";
 import { AutocompleteRef } from "./Autocomplete";
 
 interface AutocompleteProps {
+  readonly?: boolean;
   isLoading?: boolean;
   disabled?: boolean;
   defaultValue?: OptionType;
@@ -22,89 +30,99 @@ interface AutocompleteProps {
   onChangeInput?: (event: string) => void;
 }
 
-const VirtualAutocomplete = forwardRef<AutocompleteRef, AutocompleteProps>(({
-  isLoading = false,
-  disabled = false,
-  defaultValue,
-  required,
-  checkbox = false,
-  optionHi = 30,
-  options,
-  label = "",
-  noOptionsMessage = "Нет элементов",
-  groupClassName,
-  grouped = false,
-  onChange,
-  onChangeInput,
-}, ref) => {
-  const inputRef = useRef<InputRef>(null);
-  const optionsRef = useRef<VirtualListRef>(null);
-  const [isFilteredList, setIsFilteredList] = useState<boolean>(false);
-  const [filteredList, setFilteredList] = useState<OptionType[]>(options || []);
-  const [selectedOption, setSelectedOption] = useState<OptionType | null>(
-    defaultValue || null,
-  );
-  const deferredFilteredList = useDeferredValue(filteredList);
-
-  useEffect(() => {
-    if (options !== filteredList) setFilteredList(options || []);
-  }, [options]);
-
-  useImperativeHandle(ref, () => ({
-    reset() {
-      inputRef.current?.reset();
+const VirtualAutocomplete = forwardRef<AutocompleteRef, AutocompleteProps>(
+  (
+    {
+      isLoading = false,
+      readonly = false,
+      disabled = false,
+      defaultValue,
+      required,
+      checkbox = false,
+      optionHi = 30,
+      options,
+      label = "",
+      noOptionsMessage = "Нет элементов",
+      groupClassName,
+      grouped = false,
+      onChange,
+      onChangeInput,
     },
-  }));
+    ref,
+  ) => {
+    const inputRef = useRef<InputRef>(null);
+    const optionsRef = useRef<VirtualListRef>(null);
+    const [isFilteredList, setIsFilteredList] = useState<boolean>(false);
+    const [filteredList, setFilteredList] = useState<OptionType[]>(
+      options || [],
+    );
+    const [selectedOption, setSelectedOption] = useState<OptionType | null>(
+      defaultValue || null,
+    );
+    const deferredFilteredList = useDeferredValue(filteredList);
 
-  return (
-    <div className={cl.container}>
-      <Input
-        disabled={disabled}
-        isLoading={isLoading}
-        defaultValue={defaultValue}
-        required={required}
-        ref={inputRef}
-        onChange={onChange}
-        onChangeInput={onChangeInput}
-        isDefOptions={deferredFilteredList !== filteredList}
-        setFilteredList={setFilteredList}
-        filteredList={deferredFilteredList}
-        isFilteredList={isFilteredList}
-        setIsFilteredList={setIsFilteredList}
-        setSelectedOption={setSelectedOption}
-        selectedOption={selectedOption}
-        label={label}
-        options={options || []}
-        optionsRef={optionsRef}
-      />
-      {grouped ? (
-        <GroupedVirtualList
-          checkbox={checkbox}
-          optionHi={optionHi}
-          ref={optionsRef}
-          groupClassName={groupClassName || ""}
+    useEffect(() => {
+      if (options !== filteredList) setFilteredList(options || []);
+    }, [options]);
+
+    useImperativeHandle(ref, () => ({
+      reset() {
+        inputRef.current?.reset();
+      },
+    }));
+
+    return (
+      <div className={cl.container}>
+        <Input
+          readonly={readonly}
+          disabled={disabled}
+          isLoading={isLoading}
+          defaultValue={defaultValue}
+          required={required}
+          ref={inputRef}
+          onChange={onChange}
+          onChangeInput={onChangeInput}
           isDefOptions={deferredFilteredList !== filteredList}
-          groupedOptions={deferredFilteredList as GroupBase<OptionType>[]}
+          setFilteredList={setFilteredList}
+          filteredList={deferredFilteredList}
+          isFilteredList={isFilteredList}
+          setIsFilteredList={setIsFilteredList}
+          setSelectedOption={setSelectedOption}
           selectedOption={selectedOption}
-          visible={isFilteredList && !isLoading}
-          noOptionMessage={noOptionsMessage}
-          inputRef={inputRef}
+          label={label}
+          options={options || []}
+          optionsRef={optionsRef}
         />
-      ) : (
-        <VirtualList
-          checkbox={checkbox}
-          optionHi={optionHi}
-          ref={optionsRef}
-          isDefOptions={deferredFilteredList !== filteredList}
-          options={deferredFilteredList}
-          selectedOption={selectedOption}
-          visible={isFilteredList && !isLoading}
-          noOptionMessage={noOptionsMessage}
-          inputRef={inputRef}
-        />
-      )}
-    </div>
-  );
-});
+        {!readonly &&
+          (grouped ? (
+            <GroupedVirtualList
+              checkbox={checkbox}
+              optionHi={optionHi}
+              ref={optionsRef}
+              groupClassName={groupClassName || ""}
+              isDefOptions={deferredFilteredList !== filteredList}
+              groupedOptions={deferredFilteredList as GroupBase<OptionType>[]}
+              selectedOption={selectedOption}
+              visible={isFilteredList && !isLoading}
+              noOptionMessage={noOptionsMessage}
+              inputRef={inputRef}
+            />
+          ) : (
+            <VirtualList
+              checkbox={checkbox}
+              optionHi={optionHi}
+              ref={optionsRef}
+              isDefOptions={deferredFilteredList !== filteredList}
+              options={deferredFilteredList}
+              selectedOption={selectedOption}
+              visible={isFilteredList && !isLoading}
+              noOptionMessage={noOptionsMessage}
+              inputRef={inputRef}
+            />
+          ))}
+      </div>
+    );
+  },
+);
 
 export default VirtualAutocomplete;
